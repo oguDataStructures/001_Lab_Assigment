2d9adb903700646e400168c224bc372d6a9e93b9
Yasemin Gerboğa <152120171056@ogrenci.ogu.edu.tr>
2020-10-17 21:27:19 +0300

    Table wall added.

--- Blobs/Blob/main.c
+++ Blobs/Blob/main.c
@@ -1,59 +1,71 @@
 #include <stdio.h>
 #include <stdlib.h>
-#define SIZE 100
 
-	void fetchRowCol(char str[] , int *size) {
-		int i = 0;
-		char* token = strtok(str, " ");// fetch first token 
-		while (token != NULL) {
-			size[i] = atoi(token);//put the tokens into array
-			i++;
-			token = strtok(NULL, " ");
-		}
+void fetchRowCol(char str[], int* size) {
+	int i = 0;
+	char* token = strtok(str, " ");// fetch first token 
+	while (token != NULL) {
+		size[i] = atoi(token);//put the tokens into array
+		i++;
+		token = strtok(NULL, " ");
 	}
-
-
+}
+void horNumbers(int col) {
+	int number;
+	printf("\n   ");
+	for (int i = 0; i < col; i++)
+	{
+		number = i % 10;
+		printf("%d", number);
+	}
+}
+void horWall(int col) {
+	printf("\n  ");
+	printf("+");
+	for (int i = 0; i < col; i++)
+	{
+		printf("-");
+	}
+	printf("+");
+}
 void main() {
 	FILE* data;
 	int err = fopen_s(&data, "blobs1.txt", "r");
-	char dataToBeRead[SIZE][SIZE];
-	int i = 0;
-	char arr[SIZE];
+	char arr[300];
 	char* str;
 	int* size[2];
+	int row, col;
+	int* dataToBeRead;
 
 	if (data == NULL)
 	{
-		printf("Test.c file failed to open.");
+		printf("blobs1.c file failed to open.");
+		return;
 	}
-	else
-	{
-		str = fgets(arr, 300, data); //fetch the first line of file in order to reach row and columns of 2D Array.
-		fetchRowCol(str, &size); // throw the string which is include first line with first index of adress of size array 
-		int row = size[0];
-		int col = size[1];
-		printf("row:%d\ncol:%d\n", row, col);
+	str = fgets(arr, 300, data); //fetch the first line of file in order to reach row and columns of 2D Array.
+	fetchRowCol(str, &size); // throw the string which is include first line with first index of adress of size array 
+	row = size[0];
+	col = size[1];
+	dataToBeRead = (int*)malloc(row * col * sizeof(int));
+	horNumbers(col);
+	horWall(col);
+	printf("\n");
 
-		for (int i = 0; i < row; i++) {
-			printf("%d|", i);
-			for (int j = 0; j < col+1; j++) {
-				
-				
-				
-					dataToBeRead[i][j] = fgetc(data);
-					printf("%c", dataToBeRead[i][j]);
-				
-
+	for (int i = 0; i < row; i++) {
+		printf("%d |", i);
+		for (int j = 0; j < col + 1; j++) {
+			*(dataToBeRead + i * col + j) = fgetc(data);
+			printf("%c", *(dataToBeRead + i * col + j));
+			if (j == col - 1) {
+				printf("| %d", i);
 			}
-				
-	
-
 		}
 	}
-		fclose(data);			   
-		printf("\n");			   
-		system("pause");
-	
+	fclose(data);
+
+	horWall(col);
+	horNumbers(col);
+
+	printf("\n");
+	system("pause");
 }
-//dataToBeRead[l][k] = fgetc(data);
-//printf("%c", dataToBeRead[l][k]);
