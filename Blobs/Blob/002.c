97a9e89b07fde1319ac7bfe3b4580632ea1a480c
mustafa batuhan bayoglu <bayoglubatuhan@gmail.com>
2020-10-19 00:05:23 +0300

    Build up function which makes count blobs and its pixels

--- Blobs/Blob/002.c
+++ Blobs/Blob/002.c
@@ -31,21 +31,21 @@ void horWall(int col) {
 	printf("\n");
 }
 
-int BlobCount(int N, int M, int r, int c, int col, int* dataToBeRead)
+int BlobCount(int N, int M, int r, int c, int* dataToBeRead)
 {
 	if (r < 0 || r >= N || c < 0 || c >= M)
 		return 0;
-	if (*(dataToBeRead + r * col + c) == ' ')
+	if (*(dataToBeRead + r * M + c) == ' ')
 		return 0;
-	if (*(dataToBeRead + r * col + c) == NULL) {
+	if (*(dataToBeRead + r * M + c) == NULL) {
 		return 0;
 	}
 	else
 	{
-		*(dataToBeRead + r * col + c) = ' ';
+		*(dataToBeRead + r * M + c) = ' ';
 		//((data + i) + j)
-		return (1 + BlobCount(N, M, r, c - 1, col, dataToBeRead) + BlobCount(N, M, r, c + 1, col, dataToBeRead) +
-			BlobCount(N, M, r - 1, c, col, dataToBeRead) + BlobCount(N, M, r + 1, c, col, dataToBeRead));
+		return (1 + BlobCount(N, M, r, c - 1, dataToBeRead) + BlobCount(N, M, r, c + 1, dataToBeRead) +
+			BlobCount(N, M, r - 1, c, dataToBeRead) + BlobCount(N, M, r + 1, c, dataToBeRead));
 	}
 }
 
@@ -65,31 +65,31 @@ void main() {
 	}
 	else
 	{
-		str = fgets(arr, 300, data); //fetch the first line of file in order to reach row and columns of 2D Array.
+		str = fgets(arr, 300, data); //fetch the first line of file in order to reach row and columns of 2D Array which is the file.
 		fetchRowCol(str, &size); // throw the string which is include first line with first index of adress of size array 
 		int row = size[0];// assign the value of row to variable
 		int col = size[1];//assign the value of column to variable 
 		int* dataToBeRead = (int*)malloc(row * col * sizeof(int));
 
-		horNumbers(col);
-		horWall(col);
+		horNumbers(col);//Making horizontal Counter  to top of table
+		horWall(col);//Making horizontal Border to to top of table
 		for (int i = 0; i < row; i++) {
-			printf("%d|", i);
+			printf("%d|", i);//Making vertical border to left side of table
 			for (int j = 0; j < col + 1; j++) {
-				*(dataToBeRead + i * col + j) = fgetc(data);//get the character 1 by 1 and set to into 2D array
-				printf("%c", *(dataToBeRead + i * col + j));
-
+				*(dataToBeRead + i * col + j) = fgetc(data);//get the character 1 by 1 and set to into 2D dynamic allacoted array
+				printf("%c", *(dataToBeRead + i * col + j));//print to log 1 by 1 character from 2D dynamic allacoted array 
 				if (j == col - 1) {
-					printf("| %d", i);
+					printf("| %d", i);//Making vertical border to right side of table
 				}//end-if
 			}//end-for-inner
 		}//end-for-outer 
-		horWall(col);
-		horNumbers(col);
+		horWall(col);////Making horizontal Counter  to top of table
+		horNumbers(col);//Making horizontal Border to bottom of table
 		printf("\n");
+
 		for (int i = 0; i < row; i++) {
 			for (int j = 0; j < col; j++) {
-				numberPixel = BlobCount(row, col, i, j, col, dataToBeRead);
+				numberPixel = BlobCount(row, col, i, j, dataToBeRead);
 				if (numberPixel > 0)
 				{
 					blobCounter[numberBloob] = numberPixel;
